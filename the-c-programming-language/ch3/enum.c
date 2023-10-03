22d5185c9b18ba9ec48b4f2b537db6900cc70cd3
ImadDabbura <imad.dabbura@hotmail.com>
2023-10-03 10:22:36 -0500

    review: Modified ch3

--- the-c-programming-language/ch3/enum.c
+++ the-c-programming-language/ch3/enum.c
@@ -1,10 +1,6 @@
 #include <stdio.h>
 
 int main(){
-    /* enum week {Mon, Tue, Wed, Thurs, Fri, Sat, Sun}; */
-    /* printf("Mon is: %d\n", Mon); */
-    /* printf("Thurs is: %d\n", Thurs); */
-
     enum week {Mon=10, Tue, Wed=10, Thurs, Fri, Sat, Sun};
     printf("Mon is: %d\n", Mon);
     printf("Wed is: %d\n", Wed);
