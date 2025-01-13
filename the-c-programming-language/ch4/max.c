cb3a2365dbfa77f8d295ae45a0fcdf919b9fe637
ImadDabbura <imad.dabbura@hotmail.com>
2025-01-13 09:07:29 -0600

    ch4: Add max macro

--- the-c-programming-language/ch4/max.c
+++ the-c-programming-language/ch4/max.c
@@ -1,4 +1,6 @@
 /* get max of two numbers */
-int max(int a, int b){
-    return (a > b) ? a: b;
-}
+int max(int a, int b) { return (a > b) ? a : b; }
+
+/* Macro to implement max (more efficient than function call)
+ * #define max(a, b) ((a) > (b) ? (a) : (b))
+ */
--- the-c-programming-language/ch4/switch.c
+++ the-c-programming-language/ch4/switch.c
@@ -3,16 +3,16 @@
 /* if we don't have break in case block -> execution falls through
  * Note: It only falls through if one of the cases evaluates to true
  * Otherwise it won't execute any code block even if there is no break
- * in any of the cases. This means it will start executing after the firts
+ * in any of the cases. This means it will start executing after the first
  * case evaluates to true
  * */
-int main(){
-    switch (10){
-        case 10:
-            printf("Case 10\n");
-        case 1:
-            printf("Case 1\n");
-        case 2:
-            printf("Case 2\n");
-    }
+int main() {
+  switch (10) {
+  case 10:
+    printf("Case 10\n");
+  case 1:
+    printf("Case 1\n");
+  case 2:
+    printf("Case 2\n");
+  }
 }
